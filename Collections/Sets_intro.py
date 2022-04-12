6e3b6d6b2d4e07e2ca9d4201d45978041dd19af7
SadiMohamed <102523220+FromImport@users.noreply.github.com>
2022-04-12 17:17:38 +0100

    Create Sets_intro.py

---
+++ Collections/Sets_intro.py
@@ -0,0 +1,16 @@
+utensils = {"fork","knife","spoon","spatula"}
+print(utensils,type(utensils))
+
+utensils.add("garlic press")
+print(untesils)
+
+utensils.discard("spoon")
+print(utensils)
+
+rep_list = [1,2,3,4,5,6,7,8,9,9,9]
+
+print(list(set(rep_list))
+      
+x = frozonset(1,2,3,4,5,6)
+
+print(type(x))
