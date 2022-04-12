55acbbfecff5fe7f0de570dde92cd1cdf2b56edb
SadiMohamed <102523220+FromImport@users.noreply.github.com>
2022-04-12 17:29:51 +0100

    Create strings.py

---
+++ Variables/strings.py
@@ -0,0 +1,22 @@
+s = "Hello World!"
+
+
+print(s.startswith("H")
+print(s.endswith("d"))
+      
+     
+print(s.split("")
+print(s[0:4])
+print(s[0:6:2])
+
+      
+      
+      
+ print(s.capitalize())
+ print(s.lower())
+ print(s.strip("!"))
+ 
+      
+ 
+      
+      
