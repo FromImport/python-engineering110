ed038b801237cee3aedb0750cf4bd65714256464
SadiMohamed <102523220+FromImport@users.noreply.github.com>
2022-04-12 18:00:16 +0100

    Added Tuesday Learning

---
+++ Classes/animal.py
@@ -0,0 +1,11 @@
+class Animal():
+  def __init__(self,name,habitat):
+    self.name = name
+    self.habitat = habitat
+    
+  
+ def move(self):
+  return "I am moving"
+
+def breath(self):
+  return "I am breathing"
