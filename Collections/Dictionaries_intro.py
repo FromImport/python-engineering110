636e7d2d29d49bf14358296b1fe4adedff40112e
SadiMohamed <102523220+FromImport@users.noreply.github.com>
2022-04-12 17:11:03 +0100

    Create Dictionaries_intro.py

---
+++ Collections/Dictionaries_intro.py
@@ -0,0 +1,23 @@
+myself {
+  "name": "sadik",
+  "age": 21,
+  "job": "DevOps Trainee"
+}
+
+
+print(myself,type(myself))
+
+print(myself.get("name"))
+
+myself.update({"name": "sadik Mohamed","town":"London"})
+
+
+
+best_film {
+  "title": "The batman",
+  "release": 2008,
+  "category":"action/crime",
+  "rating": 10
+}
+
+  
