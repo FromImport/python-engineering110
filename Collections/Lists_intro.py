eba3754e55b3472087ffa64e22b64d303ce7e58c
SadiMohamed <102523220+FromImport@users.noreply.github.com>
2022-04-12 17:00:38 +0100

    Create Lists_intro.py

---
+++ Collections/Lists_intro.py
@@ -0,0 +1,22 @@
+colours = ['blue', 'red', 'green', 'purple', 'yellow']
+
+h = 'hello'
+print(h.upper())
+
+print(colours.append('brown'))
+print(colours)
+
+print(colours.pop())
+print(colours)
+
+colours[0] = "Lillac"
+print(colours)
+
+
+shopping_list = ["bread","chicken","milk","water"]
+
+shopping_list.append("chocolate")
+
+shopping_list.pop()
+
+shopping_list.remove("bread")
